7dc7eb752cc1d989ee4f6c5c9de382d3c7c66c40
cexen <cexenial@gmail.com>
2022-12-06 20:28:16 +0900

    add vec.dump()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fmt;
+trait Dumpable {
+    fn dump(&self);
+}
+impl<T: fmt::Display> Dumpable for Vec<T> {
+    fn dump(&self) {
+        for (i, x) in self.iter().enumerate() {
+            if i > 0 {
+                print!(" ");
+            }
+            print!("{}", x);
+        }
+        println!();
+    }
+}
+
+// --------------------
+
 #[allow(unused_imports)]
 use proconio::{fastout, input};
 
